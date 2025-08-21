b925f862799662167530937a344a9a35a7168eb5
kadeshdark265-dotcom <kadeshdark265@gmail.com>
2025-08-21 20:08:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeTheme() {
+    document.body.style.background = 
+        document.body.style.background === 'black' ? '#f0f0f0' : 'black';
+          document.body.style.color = 
+              document.body.style.color === 'white' ? '#333' : 'white';
+              }
+}
